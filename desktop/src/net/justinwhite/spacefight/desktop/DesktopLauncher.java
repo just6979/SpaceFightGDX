24af9c18a30dcba9775ec9e5fc9fce84d3fed27d
Justin White <jw@justinwhite.net>
2015-12-11 22:46:28 -0500

    Initial commit after libGDX setup.

---
+++ desktop/src/net/justinwhite/spacefight/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.justinwhite.spacefight.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.justinwhite.spacefight.SpaceFightGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceFightGame(), config);
+	}
+}
---
+++ html/src/net/justinwhite/spacefight/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.justinwhite.spacefight.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.justinwhite.spacefight.SpaceFightGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new SpaceFightGame();
+        }
+}
